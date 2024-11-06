345985825a2813eb7bec8c5255e5e36d2ecc08bc
studiobutter <bebosua05@yahoo.com.vn>
2024-11-07 00:08:26 +0700

    [Prod]: Add Embed Image

--- src/app/App.js
+++ src/app/App.js
@@ -8,6 +8,8 @@ import withRouter from "../hooks/withRouter";
 import AppRoutes from "./routes";
 import Headermain from "../header";
 import "./App.css";
+import { Helmet } from 'react-helmet';
+import EmbedImage from '../assets/embed/EmbedImage.png'
 
 function _ScrollToTop(props) {
   const { pathname } = useLocation();
@@ -21,6 +23,13 @@ const ScrollToTop = withRouter(_ScrollToTop);
 export default function App() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
+      <Helmet>
+        <meta property="og:title" content="Studio Butter" />
+        <meta property="og:description" content="Hello! I'm Butter, an artist, graphic designer and probably coder and a pianist. And this is my Profile!" />
+        <meta property="og:image" content={EmbedImage} />
+        <meta property="og:url" content="https://studiobutter.github.io/profile" />
+        <meta name="twitter:card" content="Studio Butter Website" />
+      </Helmet>
       <ScrollToTop>
         <Headermain />
         <AppRoutes />
