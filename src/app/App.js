8183507b484de947db751d18aefb54821694fb2f
studiobutter <bebosua05@yahoo.com.vn>
2024-11-07 00:29:33 +0700

    Move changes to Public. I'm stoopid :p

--- src/app/App.js
+++ src/app/App.js
@@ -8,8 +8,6 @@ import withRouter from "../hooks/withRouter";
 import AppRoutes from "./routes";
 import Headermain from "../header";
 import "./App.css";
-import { Helmet } from 'react-helmet';
-import EmbedImage from '../assets/embed/EmbedImage.png'
 
 function _ScrollToTop(props) {
   const { pathname } = useLocation();
@@ -23,13 +21,6 @@ const ScrollToTop = withRouter(_ScrollToTop);
 export default function App() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
-      <Helmet>
-        <meta property="og:title" content="Studio Butter" />
-        <meta property="og:description" content="Hello! I'm Butter, an artist, graphic designer and probably coder and a pianist. And this is my Profile!" />
-        <meta property="og:image" content={EmbedImage} />
-        <meta property="og:url" content="https://studiobutter.github.io/profile" />
-        <meta name="twitter:card" content="Studio Butter Website" />
-      </Helmet>
       <ScrollToTop>
         <Headermain />
         <AppRoutes />
