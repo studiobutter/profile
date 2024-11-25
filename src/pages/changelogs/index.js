0b87872495133957c4a7bd86349d4c4ee1be3082
studiobutter <bebosua05@yahoo.com.vn>
2024-11-26 00:37:53 +0700

    Added Changelogs page

---
+++ src/pages/changelogs/index.js
@@ -0,0 +1,36 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
+import React from "react";
+import "./style.css";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { meta } from "../../content_option";
+import { Container } from "react-bootstrap";
+
+import { changelog } from "../../changelog";
+
+export const Changelogs = () => {
+  return (
+    <HelmetProvider>
+      <Container>
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>Changelogs | {meta.title}</title>
+          <meta name="description" content={meta.description} />
+        </Helmet>
+        <h1>Changelogs</h1>
+        <br></br>
+        {changelog.map
+        ((data, i) => {
+          return (
+            <tr key={i}>
+              <h5>{data.date}</h5>
+              <p></p>
+              <p>{data.content}</p>
+            </tr>
+          );
+        })}
+        <br></br>
+        <br></br>
+      </Container>
+    </HelmetProvider>
+  );
+};
